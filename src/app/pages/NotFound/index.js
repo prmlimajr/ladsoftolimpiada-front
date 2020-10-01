91631dcc005082161e07e627d6a5e62eaba90a01
Paulo Lima <prmlimajr@hotmail.com>
2020-09-30 22:59:26 -0300

    barra de rolagem no ranking

--- src/app/pages/NotFound/index.js
+++ src/app/pages/NotFound/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import './styles.css';
+
 export default function NotFound() {
   return (
     <div>
-      <h1>404</h1>
-      <h1>Oops... Something went wrong</h1>
+      <h1 className='not-found'>404</h1>
+      <h2>Oops... Something went wrong</h2>
     </div>
   );
 }
